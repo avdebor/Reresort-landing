0f4adceb74a155d713b4fd67f85cced3f69df47c
avdebor <you@example.com>
2023-08-07 21:09:23 +0200

    deign files added

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const openButton = document.getElementById("openButton");
+const popupContainer = document.getElementById("popupContainer");
+const closeButton = document.getElementById("closeButton");
+
+openButton.addEventListener("click", () => {
+  popupContainer.classList.add("active");
+});
+
+closeButton.addEventListener("click", () => {
+  popupContainer.classList.remove("active");
+});
