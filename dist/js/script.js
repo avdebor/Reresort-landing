6b172772748985e0bfc8608751d86d9c4b78c807
avdebor <you@example.com>
2023-08-09 15:24:23 +0300

    popup window with form added

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,13 +1,27 @@
 "use strict";
 
-const openButton = document.getElementById("openButton");
 const popupContainer = document.getElementById("popupContainer");
 const closeButton = document.getElementById("closeButton");
 
-openButton.addEventListener("click", () => {
-  popupContainer.classList.add("active");
+document.querySelectorAll("#openButton").forEach((item) => {
+  item.addEventListener("click", () => {
+    popupContainer.classList.add("active");
+  });
 });
 
 closeButton.addEventListener("click", () => {
+  popupContainer.classList.remove("active");
+});
+
+//* further form functionality could be added like emailing the form content
+let form = document.getElementById("forminput");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+form.querySelector("[name='sendbtn']").addEventListener("click", () => {
+  // sending form logic might be here
+  console.log("send button triggered");
   popupContainer.classList.remove("active");
 });
